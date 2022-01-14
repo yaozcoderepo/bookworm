dcf0982f62e6cfb797df8a850a8968a6265418ee
andrewyzhao <andrewyzhao@tencent.com>
2022-01-14 04:39:01 -0500

    initial commit

---
+++ routes/api/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+    '/',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or mroe characters'
+        ).isLength({ min: 6 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // See if user exists
+
+        // Get users gravatar
+
+        // Encrypt password
+
+        // Return jsonwebtoken
+
+        res.send('User route');
+    }
+);
+
+module.exports = router;
